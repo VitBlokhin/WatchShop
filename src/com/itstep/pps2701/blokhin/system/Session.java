9b5ef7f0b0d9f3b5752c15b14e4a7c7acb95bbca
vit <vit.blokhin@gmail.com>
2017-04-08 00:27:54 +0300

    Additional method in Session, first TODO

--- src/com/itstep/pps2701/blokhin/system/Session.java
+++ src/com/itstep/pps2701/blokhin/system/Session.java
@@ -3,6 +3,7 @@ package com.itstep.pps2701.blokhin.system;
 import com.itstep.pps2701.blokhin.data.User;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 /**
  * Created by Vit on 07.04.2017.
@@ -21,13 +22,35 @@ public class Session {
     public boolean login(String name, String password){
 
         return false;
-    }
+    } // login
 
     public void logout(){
         currentUser = null;
-    }
+    } // logout
+
+    // TODO: переделать метод под один строковый аргумент
+    // сделать отдельный метод для формирования строки адреса подключения
+    // начать делать GUI - окно ввода данных бд, далее логин, далее - основное окно программы
+    // соответственно, привязать всё к логике работы сессии
+    public boolean connect(String addr, int port, String dbname){
+        boolean code = false;
+        try{
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://"
+                            + addr + ":" + port + "/" + dbname
+                            + "?autoReconnect=true&useSSL=false",
+                    "root",
+                    "");
+            code = true;
+        } catch(Exception ex) {
+            code = false;
+        } // catch
+        finally{
+            return code;
+        } // finally
+    } // connect
 
     public void disconnect(){
         conn = null;
-    }
+    } // disconnect
 } // class Session
